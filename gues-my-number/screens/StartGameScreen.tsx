19545f0a97b35d38ac62210ec1584c2634c9176d
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-11-09 21:33:51 -0400

    adding some style

--- gues-my-number/screens/StartGameScreen.tsx
+++ gues-my-number/screens/StartGameScreen.tsx
@@ -4,9 +4,20 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize='none'
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,9 +26,11 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
     marginTop: 100,
-    backgroundColor: "#72063c",
+    backgroundColor: "#39031eff",
     marginHorizontal: 24,
     borderRadius: 8,
     elevation: 4, // android
@@ -25,5 +38,23 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 }, // ios
     shadowRadius: 6, // ios
     shadowOpacity: 0.25, // ios
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    paddingBottom: 0,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
