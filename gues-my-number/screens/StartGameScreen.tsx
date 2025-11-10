91c36747b09101e7dba37c550e5c721f044a985c
Claudio Zimmermann Junior <claudio.zi.junior@gmail.com>
2025-11-09 20:12:27 -0400

    styling

--- gues-my-number/screens/StartGameScreen.tsx
+++ gues-my-number/screens/StartGameScreen.tsx
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,18 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4, // android
+    shadowColor: "black", // ios
+    shadowOffset: { width: 0, height: 2 }, // ios
+    shadowRadius: 6, // ios
+    shadowOpacity: 0.25, // ios
+  },
+});
